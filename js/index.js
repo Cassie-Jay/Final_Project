171fd7f8cd7923fbd5d13c6be3cc05458a54695d
Menyew <menye@Meaza>
2022-04-09 08:32:16 -0400

    updated html, css, js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,44 @@
-const TaskManager = {
-    console.log()
-}
+import { TaskManager } from "./taskManager.js";
+const myTaskManager = new TaskManager();
+// myTaskManager.addTask('clean my house', 'do dishes and clean my bed', 'Meaza', new Date().toDateString(), 'in progress')
+// myTaskManager.addTask('walk the do', 'clean cars', 'Simon', new Date().toDateString())
+// myTaskManager.addTask('clean my bath', 'cook ', 'Eden', new Date().toDateString())
+// myTaskManager.addTask('do laundary', 'go to gym', 'Elias', new Date().toDateString())
+const taskForm = document.getElementById('task_form');
+
+taskForm.addEventListener("submit", event => {
+    event.preventDefault();
+    let nameInput = document.getElementById('Taskname');
+    let descriptionInput = document.getElementById('Description');
+    let assignedToInput = document.getElementById('AssignedTo');
+    let statusInput = document.getElementById('status');
+    let dateInput = document.getElementById('Duedate');
+    myTaskManager.addTask(nameInput.value, descriptionInput.value, assignedToInput.value, dateInput.value, statusInput.value)
+    console.table(myTaskManager.tasks);
+    nameInput.value = "";
+    descriptionInput.value = "";
+    assignedToInput.value = "";
+    dateInput.value = "";
+});
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+  console.log('my name');
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,13 +1,21 @@
-class TaskManager {
-
-    TaskManager;
-  
-    constructor(TaskManager) {
-      this.tasks 
-    }
-  
-    introduceSelf() {
-      console.log(`Hi! I'm ${this.TaskManager}`);
-    }
-  
+export class TaskManager {
+  constructor(tasks = [], currentId = 0) {
+    this.tasks = tasks;
+    this.currentId = currentId;
   }
+  //method used to add a task to the tasks array of this class
+  addTask(name, description, assignedTo, dueDate, status = "TODO") {
+    this.currentId+=1;
+    //creat an object that pass values provided the method parameter
+    const newTask = {
+      taskId: this.currentId,
+      taskName: name,
+      taskDescription: description,
+      taskAssignedTo: assignedTo,
+      taskDueDate: dueDate,
+      
+    }
+    //add the new task object to the tasks array of the class
+   this.tasks.push(newTask) 
+  }
+}
